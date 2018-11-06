42a2fc95abc28fcf5aaeb4cde1e9d6fce97f07eb
joyet-simon <joyet.simon@hotmail.fr>
2018-11-06 17:03:34 +0100

    update 9 (héritage)

--- FractionDemo/ButtonLoop.cs
+++ FractionDemo/ButtonLoop.cs
@@ -40,12 +40,15 @@ namespace FractionDemo
 
         private void retablir(Object sender, EventArgs e)
         {
-            this.Width = Width / 2;
-            this.Height = Height / 2;
-            this.Left += Width / 2;
-            this.Top += Height / 2;
-            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BackColor = Color.LightBlue;
+            if (loupe)
+            {
+                this.Width = Width / 2;
+                this.Height = Height / 2;
+                this.Left += Width / 2;
+                this.Top += Height / 2;
+                this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                this.BackColor = Color.LightBlue;
+            }
         }
     }
 }
