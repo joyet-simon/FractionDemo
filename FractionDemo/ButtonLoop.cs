8715e3fb9655093e9c3df48ab60edd6b5a9a1167
joyet-simon <joyet.simon@hotmail.fr>
2018-11-06 16:36:18 +0100

    update 9 (héritage)

---
+++ FractionDemo/ButtonLoop.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FractionDemo
+{
+    class ButtonLoop : Button
+    {
+        public ButtonLoop(bool loupe) : base()
+        {
+            this.loupe = loupe;
+            this.MouseEnter += grossir;
+            this.MouseLeave += retablir;
+        }
+
+        public ButtonLoop() : base()
+        {
+            this.MouseEnter += grossir;
+            this.MouseLeave += retablir;
+        }
+
+        public Boolean loupe { get; set; }
+
+        private void grossir(Object sender, EventArgs e)
+        {
+            this.Left -= Width / 2;
+            this.Top -= Height / 2;
+            this.Width = Width * 2;
+            this.Height = Height * 2;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.4F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BackColor = Color.Tomato;
+        }
+
+        private void retablir(Object sender, EventArgs e)
+        {
+            this.Width = Width / 2;
+            this.Height = Height / 2;
+            this.Left += Width / 2;
+            this.Top += Height / 2;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BackColor = Color.LightBlue;
+        }
+    }
+}
--- FractionDemo/Form1.Designer.cs
+++ FractionDemo/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.buttonLoop1 = new FractionDemo.ButtonLoop();
             this.numBox1 = new FractionDemo.NumBox();
             this.SuspendLayout();
             // 
@@ -48,13 +49,25 @@
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // buttonLoop1
+            // 
+            this.buttonLoop1.BackColor = System.Drawing.Color.LightBlue;
+            this.buttonLoop1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonLoop1.Location = new System.Drawing.Point(98, 238);
+            this.buttonLoop1.loupe = false;
+            this.buttonLoop1.Name = "buttonLoop1";
+            this.buttonLoop1.Size = new System.Drawing.Size(140, 70);
+            this.buttonLoop1.TabIndex = 2;
+            this.buttonLoop1.Text = "TEST LOUPE";
+            this.buttonLoop1.UseVisualStyleBackColor = false;
+            // 
             // numBox1
             // 
-            this.numBox1.Location = new System.Drawing.Point(623, 238);
+            this.numBox1.Location = new System.Drawing.Point(513, 238);
             this.numBox1.maxi = 100D;
             this.numBox1.mini = 0D;
             this.numBox1.Name = "numBox1";
-            this.numBox1.Size = new System.Drawing.Size(100, 20);
+            this.numBox1.Size = new System.Drawing.Size(210, 20);
             this.numBox1.TabIndex = 1;
             // 
             // Form1
@@ -62,6 +75,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 444);
+            this.Controls.Add(this.buttonLoop1);
             this.Controls.Add(this.numBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -75,6 +89,7 @@
 
         private System.Windows.Forms.Button button1;
         private NumBox numBox1;
+        private ButtonLoop buttonLoop1;
     }
 }
 
