d808c0e4df5fc3b4033dc83b55bbc8367b89f5b5
joyet-simon <joyet.simon@hotmail.fr>
2018-11-07 09:54:19 +0100

    update 9 (héritage)

--- FractionDemo/Centieme.cs
+++ FractionDemo/Centieme.cs
@@ -8,35 +8,34 @@ namespace FractionDemo
 {
     class Centieme : Fraction
     {
-        public Centieme(int numerateur, int denominateur) : base()
+        public Centieme(int numerateur, int denominateur) : base(numerateur, denominateur)
         {
             if (denominateur != 100)
             {
-                this.denominateur = 100;
-                this.numerateur = numerateur * this.denominateur / denominateur;
+                base.denominateur = 100;
+                this.numerateur = numerateur * 100 / denominateur;
             }
             else
             {
-                this.denominateur = denominateur;
+                base.denominateur = 100;
                 this.numerateur = numerateur;
             }
         }
 
-        public new Int32 numerateur { get; set; }
         /// <summary>
         /// Lecture seule
         /// </summary>
         public new Int32 denominateur { get; }
-        
+
 
         public new String toText()
         {
-            return numerateur.ToString() + "\n" + "---" + "\n" + denominateur.ToString();
+            return numerateur.ToString() + "\n---\n100";
         }
 
         public override String ToString()
         {
-            return numerateur.ToString() + "/" + denominateur.ToString();
+            return base.toText();
         }
     }
 }
